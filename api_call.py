86884e72f741b63af036fc2fad7487ee693ca920
Thai Thien <ttpro1995@yahoo.com.vn>
2023-03-05 14:13:03 +0700

    wip

--- api_call.py
+++ api_call.py
@@ -8,7 +8,7 @@ load_dotenv()
 MY_API_KEY = os.getenv('MY_API_KEY')
 
 
-def get_match_id_by_puuid(puuid, page = 0):
+def get_match_id_by_puuid(puuid, page=0):
     start = 100 * page
     url = f"https://sea.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start={start}&count=100"
     headers = {
@@ -20,7 +20,24 @@ def get_match_id_by_puuid(puuid, page = 0):
         raise Exception("status code ", resp.status_code)
     return resp.json()
 
+
+def get_match(match_id):
+    url = f"https://sea.api.riotgames.com/lol/match/v5/matches/{match_id}"
+    headers = {
+        "X-Riot-Token":MY_API_KEY,
+        "User-Agent": "Meow"
+    }
+    resp = requests.get(url, headers=headers)
+    if resp.status_code != 200:
+        raise Exception("status code ", resp.status_code)
+    return resp.json()
+
+
 if __name__ == "__main__":
     match_id_list = get_match_id_by_puuid("hNgSyrUajUcwRInZQra1nBU7UcBvDcCwGMnyvQuti7HLON0CRDmGz3lIkl8LhlputovzkHF7vTE7Ag")
     print(type(match_id_list))
     print(match_id_list)
+    match_id = "VN2_40829816"
+    match = get_match(match_id)
+    print(type(match))
+    print(match["metadata"])
--- fetch-first-hundress.py
+++ fetch-first-hundress.py
@@ -1,4 +1,67 @@
 """
 
-Fetch first 100 match 
+Fetch first 100 match
 """
+import dotenv
+import os
+from dotenv import load_dotenv
+from api_call import get_match_id_by_puuid, get_match
+from tqdm import tqdm
+import json
+import time
+import jsonlines
+
+load_dotenv()
+
+MY_PUUID=os.getenv('MY_PUUID')
+DATA_MATCHES=os.getenv("DATA_MATCHES")
+
+
+############ GET SEED DATA ################################################
+
+def get_100_match_id():
+    match_id_file = os.path.join(DATA_MATCHES, "my_match_id.txt")
+    with open(match_id_file, "w") as file:
+        match_ids = get_match_id_by_puuid(puuid=MY_PUUID)
+        for match in match_ids:
+            file.write(match + "\n")
+
+def get_100_match():
+    """
+    Read from my_100_match_id.txt
+    get match
+    :return:
+    """
+    match_id_file = os.path.join(DATA_MATCHES, "my_100_match_id.txt")
+    matches_file = os.path.join(DATA_MATCHES, "my_100_matches.txt")
+    with open(match_id_file, "r") as id_file, open(matches_file, "w") as matches_file:
+        match_ids = id_file.readlines()
+        for match_id in tqdm(match_ids):
+            process_match_id = match_id.strip()
+            if len(process_match_id) > 3:
+                match_dict = get_match(process_match_id)
+                match_json = json.dumps(match_dict)
+                time.sleep(1.2)
+                matches_file.write(match_json+"\n")
+                # print("write match ", process_match_id)
+            else:
+                continue
+
+
+def get_puuid_from_100_matches():
+    """
+    read my_100_matches.txt
+    get list of puuid
+    :return:
+    """
+    puuids = []
+    matches_file = os.path.join(DATA_MATCHES, "my_100_matches.txt")
+    with jsonlines.open(matches_file) as reader:
+        for obj in tqdm(reader):
+            players_puuid = obj["metadata"]["participants"]
+            # print()
+
+##################################################################################
+
+if __name__ == "__main__":
+    get_puuid_from_100_matches()
